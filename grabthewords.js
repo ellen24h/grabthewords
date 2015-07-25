8d31d776d0f53d3b9794dc6524b34a573e7a5476
ellen24h <ellen24h@gmail.com>
2015-07-25 17:07:04 +0900

    #1 split the words

---
+++ grabthewords.js
@@ -0,0 +1,20 @@
+var originalText = "By a former marriage, Mr. Henry Dashwood had one son: by his present lady, three daughters. The son, a steady respectable young man, was amply provided for by the fortune of his mother, which had been large, and half of which devolved on him on his coming of age. By his own marriage, likewise, which happened soon afterwards, he added to his wealth. To him therefore the succession to the Norland estate was not so really important as to his sisters; for their fortune, independent of what might arise to them from their father's inheriting that property, could be but small. Their mother had nothing, and their father only seven thousand pounds in his own disposal; for the remaining moiety of his first wife's fortune was also secured to her child, and he had only a life-interest in it.";
+var net = [];
+var allWords = [];
+var theVeryWords = [];
+
+var spliting = function(originalText) {
+	allWords = text.split(" ");
+	console.log(allWords);
+}
+
+var neting = function(allWords) {
+	
+}
+
+grabbing(originalText);
+
+
+
+
+
